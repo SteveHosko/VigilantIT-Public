3946733dfbffeec4ea243fc2cbb9ffb240be1c31
SteveHosko <shosking@vigilant.it>
2016-11-02 20:09:39 +1100

    Created NewARMResGroup.ps1 in AzureAutomation.

---
+++ ASM2ARM/NewARMResGroup.ps1
@@ -0,0 +1,21 @@
+workflow NewARMResGroup
+{
+        param    ([object]$WebHookData)
+        if ($WebHookData -ne $null){
+            $reqdatabody = $WebHookData.RequestBody
+        $req = convertfrom-json $reqdatabody
+        $credname = $req.name
+        $subid = $req.subid
+        $region = $req.region
+        $resGrpName = $req.RGName
+$Creds = Get-AutomationPSCredential -Name $credname 
+$null = Login-AzureRmAccount -Environment (Get-AzureRmEnvironment -name AzureCloud) -Credential $Creds
+$null = Get-AzureRmSubscription -SubscriptionId $subID
+if(!(Get-AzureRmResourceGroup -Name $resGrpName -Location $region -ErrorAction SilentlyContinue)){
+$rg = New-AzureRmResourceGroup -Name $resGrpName -Location $region -Force}
+$out = convertto-json $rg
+write-output $out
+        }
+        else
+        {write-output "Failed to have Data entered"}
+}
